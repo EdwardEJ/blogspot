4aeaa1f01245b51f336dbdb061aae07b7baced3f
A <jesus.e.estrada@hotmail.com>
2021-02-06 16:10:21 -0600

    error handling on login

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/User';
 import { MyContext } from '../types';
-import { Resolver, Mutation, InputType, Field, Arg, Ctx } from 'type-graphql';
+import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType } from 'type-graphql';
 import argon2 from 'argon2';
 
 @InputType()
@@ -12,6 +12,24 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError{ 
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
@@ -19,25 +37,40 @@ export class UserResolver {
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ) {
-    const hasedPassword = await argon2.hash(options.password);
+    const hashedPassword = await argon2.hash(options.password);
 
     const user = em.create(User, {
       username: options.username,
-      password: hasedPassword,
+      password: hashedPassword,
     });
     await em.persistAndFlush(user);
     return user;
   }
 
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
-    const user = await em.findOne(User, { username: options.username });
+  ): Promise<UserResponse> {
+      const user = await em.findOne(User, { username: options.username, password:  });
+      if (!user) {
+          return {
+            errors: [{
+                field: 'username',
+                message: 'User does not exist'
+            }]
+          }
+      }
 
-    const hasedPassword = await argon2.hash(options.password);
-
-    return user;
+    const validPassword = await argon2.verify(user.password, options.password);
+    if (!validPassword) {
+      return {
+          errors: [{
+                field: 'password',
+                message: 'Invalid login'
+            }]
+        }
+      }
+    return {user};
   }
 }
