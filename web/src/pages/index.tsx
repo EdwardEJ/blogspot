48edc018ec15c065abc4ba0c38db2a3533b8838b
A <jesus.e.estrada@hotmail.com>
2021-02-08 20:36:55 -0600

    Implemented cursor pagination

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -7,7 +7,11 @@ import { Link } from '@chakra-ui/react';
 import React from 'react';
 
 const Index = () => {
-	const [{ data }] = usePostsQuery();
+	const [{ data }] = usePostsQuery({
+		variables: {
+			limit: 10,
+		},
+	});
 
 	return (
 		<Layout>
