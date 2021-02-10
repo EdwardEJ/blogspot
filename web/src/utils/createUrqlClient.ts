32ac1c81c7a3dfde2fdf21dcfcbd49b75d33f7d1
A <jesus.e.estrada@hotmail.com>
2021-02-10 17:23:15 -0600

    fixed cookie issue

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -81,9 +81,8 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
 	if (isSever()) {
 		cookie = ctx?.req?.headers?.cookie;
 	}
-
 	return {
-		url: 'http://localhost:4000/graphql',
+		url: process.env.NEXT_PUBLIC_API_URL as string,
 		fetchOptions: {
 			credentials: 'include' as const,
 			headers: cookie
